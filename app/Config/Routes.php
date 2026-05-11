d7c5d32d408c3059efb0572a00eb3db9246967e3
aarrizqianura <arrizqianur@gmail.com>
2026-05-11 10:00:43 +0800

    Pertemuan 3: Model dan CRUD

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -26,3 +26,14 @@ $routes->get('demo', 'Demo::index');
 
 // Route Galeri
 $routes->get('galeri', 'Galeri::index');
+
+
+// Route CRUD Buku 
+$routes->get('buku',                'Buku::index');
+$routes->get('buku/tambah',         'Buku::tambah');
+$routes->post('buku/simpan',        'Buku::simpan');
+$routes->get('buku/detail/(:num)',  'Buku::detail/$1');
+$routes->get('buku/edit/(:num)',    'Buku::edit/$1');
+$routes->post('buku/update/(:num)', 'Buku::update/$1');
+$routes->get('buku/hapus/(:num)',   'Buku::hapus/$1');
+$routes->get('buku/ekspor',         'Buku::ekspor');
--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -11,8 +11,7 @@
         href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
         rel='stylesheet'>
     <!-- Bootstrap Icons -->
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap
-icons.css'
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css'
         rel='stylesheet'>
     <!-- Custom CSS -->
     <link rel='stylesheet' href='<?= base_url('assets/css/custom.css') ?>'>
